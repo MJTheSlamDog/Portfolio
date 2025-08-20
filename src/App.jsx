e1a49800d75dadfc5bb4143ddcbcd85a0452cb44
MJTheSlamDog <gomasterjay@gmail.com>
2025-08-20 15:32:49 +0200

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Navbar from './sections/Navbar';
 
 const App = () => {
   return (
-    <div>
+    <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
+      <section id="home" className="min-h-screen" />
+      <section id="about" className="min-h-screen bg-amber-900" />
+      <section id="services" className="min-h-screen bg-amber-400" />
     </div>
   )
 }
