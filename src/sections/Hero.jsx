2c0102f5727bf23bae9edbf6838633bd0c93ad0b
MJTheSlamDog <gomasterjay@gmail.com>
2025-08-26 22:18:43 +0200

    Incomplete hero

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Hero = () => {
+    const contextRef = React.useRef(null);
+    const headerRef = React.useRef(null);
+
+    return (
+        <section
+        id='home'
+        className="flex flex-col justify-end min-h-screen">
+            <div ref={contextRef}>
+                <div style={{clipPath: "polygon(0 2%, 100% 0%, 100% 100%, 0% 100%)"}}>
+                    <div ref={headerRef} className="flex flex-col justify-center gap-12 pt-16 sm:gap-16 ">
+                        <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">
+                            404 No Bugs Found
+                        </p>
+                        <div className="px-10">
+                            <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:block">
+                                Mlandvo Nkosi
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative px-10 text-black">
+                    <div className="absolute inset-x-0 border-t-2" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero 
